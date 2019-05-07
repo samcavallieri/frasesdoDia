cf1563eaccd8458b5aa047cf403d1020e56f780a
Samuel Cavallieri <samuelcavallieri@gmail.com>
2019-05-07 16:22:49 -0300

    Initial Commit

---
+++ frasesdoDia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  frasesdoDia
+//
+//  Created by Samuel Cavallieri on 07/05/19.
+//  Copyright © 2019 Samuel Cavallieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
