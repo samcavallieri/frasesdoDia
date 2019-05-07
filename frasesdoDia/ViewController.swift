6f9aaca7918f272357d29a04a9f57bf4283bfa2a
Samuel Cavallieri <samuelcavallieri@gmail.com>
2019-05-07 19:25:03 -0300

    1 - Start project 2 - configure interface 3- create arrays 4- create let

--- frasesdoDia/ViewController.swift
+++ frasesdoDia/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBAction func novaFrase(_ sender: Any) {
+        
+        var frases: [String] = []
+        let fraseAleatoria = arc4random_uniform(3)
+        frases.append(" Se você quer fazer deus rir, conte seus planos para ele.")
+        frases.append(" Nada como cheiro de napalm no café da manhã. ")
+        frases.append(" Mantenha seus amigos perto e seus inimigos mais perto ainda. ")
+        legendaResultado.text = frases[ Int(fraseAleatoria) ]
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
